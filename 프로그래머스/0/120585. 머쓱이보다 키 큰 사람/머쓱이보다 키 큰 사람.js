5a1b420a01b2e984f89fa2866e3cf16609a67b90
sonji <133579214+sonji406@users.noreply.github.com>
2024-10-29 23:52:45 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,6 +1,6 @@
 function solution(array, height) {
     let answer = 0;
-    const heights = [...array].filter((num) => num > height);
+    const heights = array.filter((num) => num > height);
     
     answer = heights.length;
     
